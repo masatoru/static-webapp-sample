366e80c87b3ca2becf8ce749f5813e45cd83583e
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-05-21 02:55:58 +0900

    add project

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: '',
+    };
+  },
+  async created() {
+    console.log(process.env);
+    fetch('/api/getMessage?name=Vue.js')
+      .then(responce => responce.json() )
+      .then(responce => {
+        this.message = responce.message;
+      }).catch(()=>{
+        this.message = 'failed';
+      });
+  },
+});
+</script>
